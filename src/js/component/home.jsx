0092567893e7b999c0e3987e32c9f1111f1242da
sabonrex <118281670+sabonrex@users.noreply.github.com>
2023-04-28 13:43:08 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/index.css";
 
 const Home = () => {
 	const [ inputValue, setInputValue] = useState("");
 	const [ toDoItem, settoDoItem ] = useState([]);
+	
+
+	const fetchToDoItemList = async () => {
+		try {
+			const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/sabona");
+			const data = await response.json();
+			settoDoItem(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchToDoItemList();
+	}, []);
+
+	const updateList = async (newList) => {
+		try {
+			const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/sabona", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(newList)
+			});
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleAddItem = (newItem) => {
+		const newList = toDoItem.concat({ label: newItem, done: false });
+		settoDoItem(newList);
+		updateList(newList);
+	};
+
+	const handleDeleteToDo = (index) => {
+		const newList = toDoItem.filter((_, currentIndex) => index !== currentIndex);
+		settoDoItem(newList);
+		updateList(newList);
+	};
 	return (
 		<>
 		<div className="container-fluid row d-flex justify-content-center align-items-center p-2">
@@ -15,16 +58,16 @@ const Home = () => {
 						onChange={(e) => setInputValue(e.target.value)} 
 						value={inputValue}
 						onKeyPress={(e) => {
-						if (e.key === "Enter") {
-							settoDoItem(toDoItem.concat([inputValue]));
-							setInputValue("");
+							if (e.key === "Enter" && !/^\s*$/.test(inputValue)) {
+								handleAddItem(inputValue);
+								setInputValue("");
 							}
-								}}
+						}}
 							placeholder="Add Task"></input>
 					</li>
-						{toDoItem.map((item, index) => (
-						<li className="list-item">{item}
-						<button className="invisible btn btn-sm btn-default" type="button" onClick={() => settoDoItem(toDoItem.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
+						{toDoItem && toDoItem.length > 0 && toDoItem.map((item, index) => (
+						<li key={index} className="list-item">{item.label}
+						<button className="invisible btn btn-sm btn-default" type="button" onClick={() => handleDeleteToDo(index)}><i className="fas fa-trash-alt"></i></button>
 						</li>
 						))}
 			</ul>
